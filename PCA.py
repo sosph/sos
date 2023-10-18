992b544b382717db8fb6e57fca3c1aae94f3701b
sosph <121630687+sosph@users.noreply.github.com>
2023-10-18 11:15:56 +0800

    Update PCA.py

--- PCA.py
+++ PCA.py
@@ -2287,8 +2287,8 @@ def title():
                                                         VERSION 1.4 """)                                                                                                                                                                                                                                                                      
     stdout.write("             "+Fore.LIGHTRED_EX            +"        ══╦═════════════════════════════════╦══\n")
     stdout.write("             "+Fore.LIGHTRED_EX+"╔═════════╩═════════════════════════════════╩═══════════════════════════╗\n")
-    stdout.write("             "+Fore.LIGHTRED_EX+"║ "+Fore.LIGHTWHITE_EX   +"    FB Page : SOSPH / Kheirbhen Lee"+Fore.LIGHTRED_EX  +" ║\n")
-    stdout.write("             "+Fore.LIGHTRED_EX+"║ "+Fore.LIGHTWHITE_EX   +"    Copyright : Anonymous Philippines   "+Fore.LIGHTRED_EX  +"                          ║\n")
+    stdout.write("             "+Fore.LIGHTRED_EX+"║ "+Fore.LIGHTWHITE_EX   +"    FB Page : SOSPH / Kheirbhen Lee"+Fore.LIGHTRED_EX  +"                                   ║\n")
+    stdout.write("             "+Fore.LIGHTRED_EX+"║ "+Fore.LIGHTWHITE_EX   +"    Copyright : Anonymous Philippines   "+Fore.LIGHTRED_EX  +"                              ║\n")
     stdout.write("             "+Fore.LIGHTRED_EX+"║ "+Fore.LIGHTWHITE_EX   +"    Contact   : 09212121212  "+Fore.LIGHTRED_EX  +"                                         ║\n")
     stdout.write("             "+Fore.LIGHTRED_EX+"║ "+Fore.LIGHTWHITE_EX   +"    COMMAND   : 'help'       "+Fore.LIGHTRED_EX  +"                                         ║\n")
 
@@ -2296,7 +2296,7 @@ def title():
     stdout.write("\n")
 ##############################################################################################
 def command():
-    stdout.write(Fore.LIGHTRED_EX+"╔═══"+Fore.LIGHTRED_EX+"[""PCA"+Fore.LIGHTRED_EX+"@"+Fore.LIGHTRED_EX+"Ph"+Fore.CYAN+"]"+Fore.LIGHTRED_EX+"\n╚══\x1b[38;2;0;255;189m> "+Fore.WHITE)
+    stdout.write(Fore.LIGHTRED_EX+"╔═══"+Fore.LIGHTRED_EX+"[""sosph"+Fore.GREEN+"@"+Fore.LIGHTRED_EX+"root"+Fore.CYAN+"]"+Fore.LIGHTRED_EX+"\n╚══\x1b[38;2;0;255;189m> "+Fore.WHITE)
     command = input()
     if command == "cls" or command == "clear":
         clear()
